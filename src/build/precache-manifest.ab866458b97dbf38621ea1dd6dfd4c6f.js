069e3ca480945e08c2bbf9d196cd79e3fd38a364
Jake Posner <jposner91@gmail.com>
2019-12-28 17:39:35 -0500

    first commit

---
+++ src/build/precache-manifest.ab866458b97dbf38621ea1dd6dfd4c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62a58ab51c982005731d96ae262addb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2807d9485e59df35af1",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "5450f749edc9bcdf10be",
+    "url": "/static/js/2.a9cc7312.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a9cc7312.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2807d9485e59df35af1",
+    "url": "/static/js/main.2808d802.chunk.js"
+  },
+  {
+    "revision": "0cd6605b9f1c13a9350e",
+    "url": "/static/js/runtime-main.da482c81.js"
+  }
+]);
