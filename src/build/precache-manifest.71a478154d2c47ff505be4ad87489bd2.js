5414277e88948c3a4d4849391a0972a94c2f4cca
Jake Posner <jposner91@gmail.com>
2019-12-29 14:42:08 -0500

    back to my code

---
+++ src/build/precache-manifest.71a478154d2c47ff505be4ad87489bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29274e5e475576e4dc9b622fe0d0c5ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c64f8a882da3801d319",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "13e55a6b11d74692ce5f",
+    "url": "/static/js/2.0afd3d31.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0afd3d31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c64f8a882da3801d319",
+    "url": "/static/js/main.c0eb49d8.chunk.js"
+  },
+  {
+    "revision": "0cd6605b9f1c13a9350e",
+    "url": "/static/js/runtime-main.da482c81.js"
+  }
+]);
