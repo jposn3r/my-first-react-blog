878b5e5ecb5372e0623c38aa4bad0fc7d15bfaf2
Jake Posner <jposner91@gmail.com>
2019-12-29 14:38:22 -0500

    testing build from course files

---
+++ src/build/precache-manifest.7c270e687d17c5173ad9308f6bbade75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a579e00bd113c1dfa1c4",
+    "url": "/static/js/main.44d0eff6.chunk.js"
+  },
+  {
+    "revision": "1410f67aa89e49b2ba06",
+    "url": "/static/js/2.a5ce7fec.chunk.js"
+  },
+  {
+    "revision": "a579e00bd113c1dfa1c4",
+    "url": "/static/css/main.f8d9cfc0.chunk.css"
+  },
+  {
+    "revision": "c18af30360fc16e70b0262b0412c9201",
+    "url": "/index.html"
+  }
+];
